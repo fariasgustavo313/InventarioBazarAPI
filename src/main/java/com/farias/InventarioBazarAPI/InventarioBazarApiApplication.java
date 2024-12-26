174af305eedaf39e859e7015c14a4c84ac38c8c9
Farias Gustavo <fariasgustavo313@gmail.com>
2024-12-26 14:16:33 -0300

    comienzo de proyecto de gestion

---
+++ src/main/java/com/farias/InventarioBazarAPI/InventarioBazarApiApplication.java
@@ -0,0 +1,13 @@
+package com.farias.InventarioBazarAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioBazarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioBazarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farias/InventarioBazarAPI/InventarioBazarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farias.InventarioBazarAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioBazarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
